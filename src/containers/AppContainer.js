08adb71dad875947602867fdbb76b8e0c4f328ce
igor.grebenev@quantumsoft.ru <igor.grebenev@quantumsoft.ru>
2018-04-11 10:48:10 +0700

    Side menu updated

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -16,8 +16,8 @@ export default class AppContainer extends Component {
       <div id="mySidenav" className="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="#">About</a>
-        <a href="#">Services</a>
-        <a href="#">Clients</a>
+        <a href="#">Multiplication table</a>
+        <a href="#">Arithmetic ...</a>
         <a href="#">Contact</a>
       </div>
       <div>

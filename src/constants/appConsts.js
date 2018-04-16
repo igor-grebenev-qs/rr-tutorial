e35fa059d1fa1cb5c50e416d3dd9496e85f6487a
igor.grebenev@quantumsoft.ru <igor.grebenev@quantumsoft.ru>
2018-04-16 22:19:59 +0700

    main menu started

---
+++ src/constants/appConsts.js
@@ -0,0 +1,2 @@
+export const MULT_APP = 'MULT_APP'
+export const MENAR_APP = 'MENAR_APP'
--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {MULT_APP, MENAR_APP} from '../constants/appConsts'
 import Menar from './Menar'
 import MultiplicationTestTable from './MultiplicationTestTable'
 require('../assets/styles/sidebar.less')
@@ -7,23 +8,30 @@ export default class AppContainer extends Component {
   constructor (props) {
     super(props)
     this.state = {
-      opened: true
+      opened: true,
+      currentApp: MULT_APP
     }
   }
 
+  setApp (app) {
+    console.warn('SET APPLICATION', app)
+    this.setState({currentApp: app})
+  }
+
   render() {
+    const {state} = this
     return (
     <div>
       <div id="mySidenav" className="sidenav">
         <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
         <a href="#">About</a>
-        <a href="#">Multiplication table</a>
-        <a href="#">Arithmetic ...</a>
+        <a href="#" onClick={ () => {this.setApp(MULT_APP)}}>Multiplication table</a>
+        <a href="#" onClick={ () => {this.setApp(MENAR_APP)}}>Arithmetic ...</a>
         <a href="#">Contact</a>
       </div>
       <div>
-        {/*<MultiplicationTestTable />*/}
-        <Menar/>
+        {state.currentApp === MULT_APP && <MultiplicationTestTable />}
+        {state.currentApp === MENAR_APP && <Menar/>}
       </div>
     </div>)
   }
